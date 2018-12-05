45a0eb17f8280eeb0bca8832e969bd9c1c18b41d
Ben Mulvey <ben@mulveyfamily.com>
2018-12-05 14:09:05 -0500

    start of day 5

---
+++ 5/script_part1.py
@@ -0,0 +1,20 @@
+with open('input.txt', 'r') as file:
+    line = file.readlines()[0].strip('\n')
+
+
+def check_case(char0, char1):
+    if char0.lower() != char1.lower():
+        return False
+    if char0.islower() and char1.isupper():
+        return True
+    elif char0.isupper() and char1.islower():
+        return True
+
+
+for idx, i in enumerate(line):
+    if idx + 1 == len(line):
+        break
+    print(i + ' ' + line[idx+1])
+    print(check_case(i, line[idx+1]))
+
+
